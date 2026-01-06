b6c3d3088dcd2aa67e63689ce6aeadd8dae44930
Alberto <Alberto@LAPTOP-UV2JF5U0>
2026-01-06 20:01:09 +0100

    06/01/26

--- prPractica08/src/prPractica08/Ejercicio9.java
+++ prPractica08/src/prPractica08/Ejercicio9.java
@@ -9,7 +9,24 @@ public class Ejercicio9 {
 
 	public static void main(String[] args) {
 		
-		
+		int [] array = new int [15];
+
+		//Rellenar el array con numeros aleatorios
+		for(int i=0;i<array.length;i++) {
+			array[i]=(int)(Math.random()*100);
+			System.out.print(array[i]+" ");
+
+		}
+
+		System.out.println();
+		int aux=0;
+
+		for(int i=0;i<array.length;i++) {
+			aux = array[i];
+			array[i]=array[array.length-1-i];
+			System.out.print(array[i]+" ");
+			array[array.length-1-i]=aux;
+		}
 		
 	}
 
